4e3a0ce9c9783f4f70c2c51fda7b911e8907befb
Stephanie Raad <stephy.raad@gmail.com>
2025-03-20 15:55:29 -0400

    [FEATURE]: Add xlabel and ylabel to graphs

--- app.py
+++ app.py
@@ -12,16 +12,43 @@ if st.checkbox('Show only automatic transmission'):
     df = df[df['transmission'] == 'automatic']
 
     # Update plots with filtered data
-    fig_hist = px.histogram(df, x='price', title='Price Distribution (Automatic only)')
+    fig_hist = px.histogram(df,
+      x='price',
+      title='Price Distribution (Automatic only)',
+      labels={'price': 'Price'}
+    )
+    fig_hist.update_layout(yaxis_title="Price Distribution")
     st.plotly_chart(fig_hist)
 
-    fig_scatter = px.scatter(df, x='model_year', y='price', title='Price vs. Model Year (Automatic only)')
+    fig_scatter = px.scatter(df,
+      x='model_year',
+      y='price',
+      title='Price vs. Model Year (Automatic only)',
+      labels={
+        'model_year': 'Model Year',
+        'price': 'Price',
+        }
+      )
     st.plotly_chart(fig_scatter)
+
 else:
   # Histogram of price distribution
-  fig_hist = px.histogram(df, x='price', title='Price Distribution')
+  fig_hist = px.histogram(df,
+    x='price',
+    title='Price Distribution (Automatic only)',
+    labels={'price': 'Price'}
+  )
+  fig_hist.update_layout(yaxis_title="Price Distribution")
   st.plotly_chart(fig_hist)
 
   # Scatter plot of price vs. model year
-  fig_scatter = px.scatter(df, x='model_year', y='price', title='Price vs. Model Year')
+  fig_scatter = px.scatter(df,
+    x='model_year',
+    y='price',
+    title='Price vs. Model Year (Automatic only)',
+    labels={
+      'model_year': 'Model Year',
+      'price': 'Price',
+      }
+    )
   st.plotly_chart(fig_scatter)
